e2db227db6b8fc2c2e64e171b41fc4e53ce80308
iggybo <igorbobar@gmail.com>
2016-01-18 16:06:37 +0100

    Update MainActivity.java

--- app/src/main/java/com/igorbobar/m2016/MainActivity.java
+++ app/src/main/java/com/igorbobar/m2016/MainActivity.java
@@ -29,40 +29,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentview(R.layout.activity_main);
-        
-        Button btnMap = (Button) findViewById(R.id.btnMap);
-        btnMap.setOnClickListener((v) -> {
-            if(mapReady)
-                n_map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        });
-        Button btnSatellite = (Button) findViewById(R.id.btnSatellite);
-        btnSatellite.setOnClickListener((v) -> {
-            if(mapReady)
-                n_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        });
-        Button btnHybrid = (Button) findViewById(R.id.btnHybrid);
-        btnHybrid.setOnClickListener((v) -> {
-            if(mapReady)
-                n_map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        });
-        
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap map){
-        mapReady=true;
-        m_map = map;
-        LatLng munich = new LatLng(48.14965999999999,11.567860200000041);
-        CameraPosition target = CameraPosition.builder().target(munich).zoom(17).build();
-        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(Target));
-    }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
