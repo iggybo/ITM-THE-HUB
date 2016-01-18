ab1c828ab59657433a760c8d17d7cb291ae9c496
iggybo <igorbobar@gmail.com>
2016-01-18 16:06:02 +0100

    Update MapFragment.java

--- app/src/main/java/com/igorbobar/m2016/MapFragment.java
+++ app/src/main/java/com/igorbobar/m2016/MapFragment.java
@@ -1,1 +1,33 @@
+package com.igorbobar.m2016;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.format.Time;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
