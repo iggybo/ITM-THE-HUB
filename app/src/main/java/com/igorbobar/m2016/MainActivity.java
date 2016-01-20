739b59db2e89222b31413575028e252965cf72e8
Igor Bobar <igorbobar@gmail.com>
2016-01-20 12:02:12 +0100

    Update MainActivity.java

--- app/src/main/java/com/igorbobar/m2016/MainActivity.java
+++ app/src/main/java/com/igorbobar/m2016/MainActivity.java
@@ -1,9 +1,14 @@
 package com.igorbobar.m2016;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager; 
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log; 
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
