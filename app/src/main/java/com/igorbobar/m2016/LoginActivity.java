92027e73183988a6f87c7327b36804e825fe676e
Igor Bobar <igorbobar@gmail.com>
2016-01-21 13:44:17 +0100

    Update LoginActivity.java

--- app/src/main/java/com/igorbobar/m2016/LoginActivity.java
+++ app/src/main/java/com/igorbobar/m2016/LoginActivity.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
 
 public class LoginActivity extends AsyncTask <Integer, String[]> {
-   {
-    
-  }
+   
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.login_detail);
+   
+   }
+  
 }
