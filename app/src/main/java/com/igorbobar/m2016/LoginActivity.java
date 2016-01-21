e02c312f9d6f05952bf38e13cf8df1bd239cad23
Igor Bobar <igorbobar@gmail.com>
2016-01-21 14:43:59 +0100

    Update LoginActivity.java

--- app/src/main/java/com/igorbobar/m2016/LoginActivity.java
+++ app/src/main/java/com/igorbobar/m2016/LoginActivity.java
@@ -9,8 +9,9 @@ public class LoginActivity extends AsyncTask <Integer, String[]> {
       mGoogleApiClient = new GoogleApiClient.Builder(this)
                .enableAutoManage(this, this)
                .build();
-               
-   
+      LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+      loginButton.setSize(LoginButton.SIZE_STANDARD);
+      loginButton.setScopes(gso.getScopeArray());
    }
   
 }
