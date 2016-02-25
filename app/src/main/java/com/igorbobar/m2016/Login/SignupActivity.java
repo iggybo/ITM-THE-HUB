dbd375d0cf542f9405ed56b5ee58759426a6d208
Igor Bobar <igorbobar@gmail.com>
2016-02-25 13:01:29 +0100

    Create SignupActivity.java

---
+++ app/src/main/java/com/igorbobar/m2016/Login/SignupActivity.java
@@ -0,0 +1,19 @@
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class SignupActivity extends AppCompatActivity {
+  private static final String TAG = "SignupActivity";
+  
+  @InjectView(R.
+  @InjectView(R.
+  @InjectView(R.
