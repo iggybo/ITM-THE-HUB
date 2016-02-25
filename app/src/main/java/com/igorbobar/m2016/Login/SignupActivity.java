cd8f5e1b5de8a8273be8ebd5449e2acbf23242a6
Igor Bobar <igorbobar@gmail.com>
2016-02-25 14:31:52 +0100

    Update SignupActivity.java

--- app/src/main/java/com/igorbobar/m2016/Login/SignupActivity.java
+++ app/src/main/java/com/igorbobar/m2016/Login/SignupActivity.java
@@ -25,7 +25,7 @@ public class SignupActivity extends AppCompatActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
-      setContentView(R.layout.signup_activity);
+      setContentView(R.layout.activity_signup);
       ButterKnife.inject(this);
       
       _signupButton.setOnClickListener(new View.OnClickListener() {
