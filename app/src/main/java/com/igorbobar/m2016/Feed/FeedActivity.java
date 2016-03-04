fc161638a7fa51510ac2d04c74cdefe20cb63cd1
Igor Bobar <igorbobar@gmail.com>
2016-03-04 15:17:00 +0100

    Update and rename app/src/main/java/com/igorbobar/m2016/NewsFeed/NewsFeedActivity.java to app/src/main/java/com/igorbobar/m2016/Feed/FeedActivity.java

---
+++ app/src/main/java/com/igorbobar/m2016/Feed/FeedActivity.java
@@ -0,0 +1,20 @@
+package com.igorbobar.m2016.Feed;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FeedActivity extends MainActivity {
+  
+}
