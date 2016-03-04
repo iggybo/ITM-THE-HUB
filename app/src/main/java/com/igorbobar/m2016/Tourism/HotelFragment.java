4400b3013483dc9f3547abeccdb93ef69609311e
Igor Bobar <igorbobar@gmail.com>
2016-03-04 15:06:45 +0100

    Update and rename HotelLocationFragment.java to HotelFragment.java

--- app/src/main/java/com/igorbobar/m2016/Tourism/HotelFragment.java
+++ app/src/main/java/com/igorbobar/m2016/Tourism/HotelFragment.java
@@ -15,6 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class HotelLocationFragment extends TourismActivity {
+public class HotelFragment extends TourismActivity {
   
 }
