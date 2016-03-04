16a33216001520653218ef2598161a0b8b096460
Igor Bobar <igorbobar@gmail.com>
2016-03-04 15:52:49 +0100

    Update Utility.java

--- app/src/main/java/com/igorbobar/m2016/Utilities/Utility.java
+++ app/src/main/java/com/igorbobar/m2016/Utilities/Utility.java
@@ -1,11 +1,13 @@
+package com.igorbobar.m2016.Utilities;
 
+public class Utility { 
 
-static public boolean isNetworkAvailable(Context c) {
-    ConnectivityManager cm = 
-            (ConnectivityManager)c.getSystemService(Context.CONNECTIVITY_SERVICE);
+    static public boolean isNetworkAvailable(Context c) {
+        ConnectivityManager cm = 
+                (ConnectivityManager)c.getSystemService(Context.CONNECTIVITY_SERVICE);
             
-    NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-    return activeNetwork != null &&
-            activeNetwork.isConnectedOrConnecting();
-  }
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+    } 
 }
