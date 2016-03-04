1b8695954626d9f30c84fac7ed51b5656bec3d2e
Igor Bobar <igorbobar@gmail.com>
2016-03-04 14:50:47 +0100

    Update TourismActivity.java

--- app/src/main/java/com/igorbobar/m2016/Tourism/TourismActivity.java
+++ app/src/main/java/com/igorbobar/m2016/Tourism/TourismActivity.java
@@ -1,1 +1,27 @@
+package com.igorbobar.m2016.tourism;
 
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+//Redirected from MainActivity, this is where the section concerning tourism-related activities start
+
+public class TourismActivity extends MainActivity {
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_tourism);
+  }
+}
